26611952db635c8cf5e3c969011cefdf8905be3a
AndreDyrstad <dyrstad.a@gmail.com>
2023-01-10 19:23:11 +0100

    Added cost calculator

--- src/components/discount/discount-picker/DiscountPicker.js
+++ src/components/discount/discount-picker/DiscountPicker.js
@@ -1,9 +1,11 @@
-import React, {useState } from "react";
+import React, { useState } from "react";
+import { Button } from "react-bootstrap";
 import TagGrid from "../tag-grid/TagGrid";
 
 export default function DiscountPicker(props) {
 
     const [discount, setDiscount] = useState(0)
+    const [baseCost, setBaseCost] = useState(0)
 
     let calculateDiscount = activeTags => {
         let sum = 0
@@ -18,7 +20,15 @@ export default function DiscountPicker(props) {
     return (
         <div>
             <TagGrid multiple={true} tags={props.tags} calculateDiscount={calculateDiscount} />
+            <div>
+                <Button className="DiscountButton" onClick={() => setBaseCost(baseCost + 1)}>+1</Button>
+                <Button className="DiscountButton" onClick={() => setBaseCost(baseCost + 5)}>+5</Button>
+                <Button className="DiscountButton" onClick={() => setBaseCost(baseCost + 10)}>+10</Button>
+                <Button variant="danger" className="DiscountButton" onClick={() => setBaseCost(0)}>Reset</Button>
+            </div>
+            <p>Base cost: {baseCost}</p>
             <p>Discount: {discount}</p>
+            <p>Final cost: {Math.max(baseCost - discount, 0)} </p>
         </div>
     )
 }
--- src/components/discount/tag-picker/TagPicker.js
+++ src/components/discount/tag-picker/TagPicker.js
@@ -18,12 +18,12 @@ export default function TagPicker(props) {
                 <TagGrid multiple={false} tags={props.tags} setActive={setActive} />
             </div>
             <div>
-                <Button onClick={() => { addDiscount(1); props.showWindow(false) }}>-1</Button>
-                <Button onClick={() => { addDiscount(2); props.showWindow(false) }}>-2</Button>
-                <Button onClick={() => { addDiscount(3); props.showWindow(false) }}>-3</Button>
-                <Button onClick={() => { addDiscount(4); props.showWindow(false) }}>-4</Button>
-                <Button onClick={() => props.showWindow(false)}>Cancel</Button>
+                <Button className="DiscountButton" onClick={() => { addDiscount(1); props.showWindow(false) }}>-1</Button>
+                <Button className="DiscountButton" onClick={() => { addDiscount(2); props.showWindow(false) }}>-2</Button>
+                <Button className="DiscountButton" onClick={() => { addDiscount(3); props.showWindow(false) }}>-3</Button>
+                <Button className="DiscountButton" onClick={() => { addDiscount(4); props.showWindow(false) }}>-4</Button>
             </div>
+            <Button className="CancelButton" variant="danger" onClick={() => props.showWindow(false)}>Cancel</Button>
         </div>
     )
 }
